a4238b3212b25b0e5c455c1bd0cf11545d66b992
darwannneee <darwanchannel1@gmail.com>
2024-03-08 01:24:36 +0700

    update new responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Banner from "../assets/img/Home/banner.png"
 import Banner2 from "../assets/img/Home/banner2.png"
 import Banner3 from "../assets/img/Home/Banner3.png"
+import StressCapital from "../assets/img/Home/stress.png"
 // import Navbar from "./Navbar";
 
 // Import Image
@@ -11,57 +12,65 @@ import TwitterImage from "../assets/img/Home/twitter.png"
 import localFont from "@next/font/local";
 const myFont = localFont({src : '../assets/font/HelveticaMedExt.ttf'})
 
+
 export default function Home() {
   return (
     /* banner new 1 */
     <main className={myFont.className}>
-        <div className="w-full h-screen bg-center bg-cover left-0 top-0" style={{backgroundImage: `url(${Banner.src})`}}>
-                {/* <Navbar /> */}
-
-                {/* Social Media */}
-                <div className="grid grid-cols-3 place-items-center pt-[570px] 2xl:pt-[950px] mx-80">
-                    <div className="w-[72px] h-[72px] 2xl:w-[130px] 2xl:h-[130px] rounded-full bg-[#D9D9D9] grid place-items-center">
-                        <img src={GithubImage.src} alt="Github" className="2xl:w-20" />
+        <div className="w-full h-screen bg-center bg-cover md:left-0 md:top-0" style={{backgroundImage: `url(${Banner.src})`}}>
+            <div>
+            <div className="absolute inset-0 flex items-center justify-center">
+                <img src={StressCapital.src} className="max-h-full max-w-full" alt="Gambar"/>
+            </div>
+                {/* Button */}
+                <div className="absolute bottom-20 md:bottom-16 left-10 right-10 md:left-96 md:right-96 mx-auto grid grid-cols-3 place-items-center">
+                    <div className="w-16 h-16 md:w-20 md:h-20 rounded-full bg-[#D9D9D9] grid place-items-center">
+                        <img src={GithubImage.src} alt="Github" className="w-10  md:w-14" />
                     </div>
-                    <div className="w-[72px] h-[72px] 2xl:w-[130px] 2xl:h-[130px] rounded-full bg-[#D9D9D9] grid place-items-center">
-                        <img src={DiscordImage.src} alt="Discord"  className="w-16" />
+                    <div className="w-16 h-16 md:w-20 md:h-20 rounded-full bg-[#D9D9D9] grid place-items-center md:mt-0">
+                        <img src={DiscordImage.src} alt="Discord" className="w-12 md:w-14" />
                     </div>
-                    <div className="w-[72px] h-[72px] 2xl:w-[130px] 2xl:h-[130px] rounded-full bg-[#D9D9D9] grid place-items-center">
-                        <img src={TwitterImage.src} alt="Twitter"  className="w-12" />
+                    <div className="w-16 h-16 md:w-20 md:h-20 rounded-full bg-[#D9D9D9] grid place-items-center md:mt-0">
+                        <img src={TwitterImage.src} alt="Twitter" className="w-10 md:w-10" />
                     </div>
-                </div>
-        </div>
-        <div className="w-full h-screen bg-center bg-cover left-0 top-0 bg-black" style={{backgroundImage: `url(${Banner2.src})`}}>
-                <div className="pt-56">
-                    <div className="mx-[440px] text-white">
-                        <h1 className="text-5xl">what is</h1>
-                        <h1 className="text-8xl">STRESS <span className="text-[#CF5BFF]">CAPITAL?</span></h1>
-                    </div>
-                    <div className="mx-[210px] text-white pt-5">
-                        <h1>Stress Capital, headquartered in Indonesia, is a dynamic decentralized autonomous organization (DAO) specializing in the crypto and NFT sectors. Our primary goal is to facilitate the inclusion of Indonesian individuals into the cryptocurrency space by offering educational resources on the broader crypto ecosystem and fostering participation in international projects. Our diverse team of 32 professionals, including the founder, admin, moderator, and collaboration manager, works cohesively to achieve these objectives.</h1>
-                        <button className="w-[165px] h-[46.31px] bg-[#CF5BFF] bg-opacity-20 mx-96 mt-3 rounded-xl">See More</button>
-                    </div>      
-                </div>
-        </div>
-        <div className="w-full h-screen bg-center bg-cover left-0 top-0" style={{backgroundImage: `url(${Banner3.src})`}}>
-            <div className="mx-24 text-white pt-10">
-                <h1 className="text-[25px] font-bold text-center">Latest News</h1>
-                <div className="pt-8 mx-60">
-                    <h1 className=" text-[16px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet iusto quod eius voluptas maxime blanditiis officia reiciendis dolorem accusamus?</h1>
-                    <div className="flex pt-2">
-                        <h1 className="text-[12px] text-[#CF5BFF] ">Darwan | Posted 12hrs ago</h1> 
-                        <hr className="border-t-2 border-[#CF5BFF] ml-4 w-[490px] my-auto" />  
-                    </div>
-
-                    <button className="w-[165px] h-[46.31px] bg-[#CF5BFF] bg-opacity-20 mx-72 mt-6">See More</button>
-                </div>
-                <div className="pt-52 text-center">
-                    <h1 className="text-[38px] mx-16 leading-10">&quot;Do you want to be right or do you want to make money ?&quot;</h1>
-                    <h1 className="text-[#CF5BFF] text-[24px] pt-3">Darwan 2023</h1>
                 </div>
             </div>
-                {/* <Navbar /> */}
         </div>
+    <div className="flex items-center justify-center w-full h-screen bg-center bg-cover bg-black" style={{backgroundImage: `url(${Banner2.src})`}}>
+        <div className="">
+            <div className="mx-6 md:mx-[440px] text-white">
+                <h1 className="text-3xl md:text-5xl">What is</h1>
+                <h1 className="text-5xl md:text-8xl">STRESS <span className="text-[#CF5BFF]">CAPITAL?</span></h1>
+            </div>
+            <div className="mx-6 md:mx-[210px] text-white pt-5">
+                <h1 className="text-xs md:text-base">Stress Capital, headquartered in Indonesia, is a dynamic decentralized autonomous organization (DAO) specializing in the crypto and NFT sectors. Our primary goal is to facilitate the inclusion of Indonesian individuals into the cryptocurrency space by offering educational resources on the broader crypto ecosystem and fostering participation in international projects. Our diverse team of 32 professionals, including the founder, admin, moderator, and collaboration manager, works cohesively to achieve these objectives.</h1>
+                <div className="text-center">
+                    <button className="w-32 md:w-[165px] h-10 md:h-[46.31px] bg-[#CF5BFF] bg-opacity-20 mt-3 rounded-xl ">See More</button>
+                </div>
+               
+            </div>      
+        </div>
+    </div>
+    <div className="w-full h-screen bg-center bg-cover left-0 top-0" style={{backgroundImage: `url(${Banner3.src})`}}>
+        <div className="mx-4 md:mx-24 text-white pt-24">
+            <h1 className="text-2xl md:text-[25px] font-bold text-center">Latest News</h1>
+            <div className="pt-4 md:pt-8 mx-4 md:mx-60">
+                <h1 className="text-sm md:text-[16px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet iusto quod eius voluptas maxime blanditiis officia reiciendis dolorem accusamus?</h1>
+                <div className="flex pt-2">
+                    <h1 className="text-xs md:text-[12px] text-[#CF5BFF]">Darwan | Posted 12hrs ago</h1> 
+                    <hr className="border-t-2 border-[#CF5BFF] ml-4 w-[490px] my-auto" />  
+                </div>
+                <div className="flex justify-center mt-2 md:mt-6">
+                    <button className="w-32 md:w-[165px] h-10 md:h-[46.31px] bg-[#CF5BFF] bg-opacity-20 rounded-xl">See More</button>
+                </div>
+            </div>
+            <div className="pt-28 md:pt-52 text-center">
+                <h1 className="text-4xl md:text-[38px] mx-4 md:mx-16 leading-10">&quot;Do you want to be right or do you want to make money ?&quot;</h1>
+                <h1 className="text-2xl md:text-[24px] text-[#CF5BFF] pt-3">Darwan 2023</h1>
+            </div>
+        </div>
+    </div>
+
    </main>
 
 )
